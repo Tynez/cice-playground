409a0b6437a30538c299f277ab866a93d66d39c3
Tynez <alejandro.mtnez13@gmail.com>
2021-01-09 23:11:11 +0100

    Add create task

--- packages/10-reactjs/todo-list/src/my-solutions/features/app.tsx
+++ packages/10-reactjs/todo-list/src/my-solutions/features/app.tsx
@@ -1,5 +1,23 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 export const App: FC = () => {
-  return (<></>)
+  const [taskTitle, setTaskTitle] = useState('')
+  const [todoList, setTodoList] = useState<string[]>([])
+
+  const addTask = () => {
+    setTodoList([...todoList, taskTitle])
+    setTaskTitle('')
+  }
+  return (
+    <>
+      <h1>Todo List</h1>
+      <input type="text" value={taskTitle} onChange={e => setTaskTitle(e.target.value)} />
+      <button onClick={addTask}>Add task</button>
+      <ul>
+        {todoList.map((task, key) => (
+          <li key={key}>{task}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
